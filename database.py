f0978471e8dba9243bd20fbf2526ade5652cc593
Kristawan Jaroenrad <kristawanja@gmail.com>
2023-11-27 00:24:41 +0700

    Add all the required functions except each role's code

--- database.py
+++ database.py
@@ -1,21 +1,102 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
-import csv, os
+import csv, os, copy
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+class CSV_reader():
+    def __init__(self):
+        self.__location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        
+    def read_data_from_file(self, file_name):
+        """
+        :param: file_name : file's name
+        :return: a list of a datas in the csv file specified.
+        """
+        data = []
+        with open(os.path.join(self.__location__, file_name)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                data.append(dict(r))
+        return data
 
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+
 
 # add in code for a Database class
+class DB:
+    def __init__(self):
+        self.__database = []
+
+    def insert(self, table):
+        self.__database.append(table)
+
+    def delete(self, table_name):
+        for table_num in range(len(self.__database)):
+            if self.__database[table_num].table_name == table_name:
+                self.__database.pop(table_num)
+                return 1
+        return None
+
+    def search(self, table_name):
+        for table in self.__database:
+            if table.table_name == table_name:
+                return table
+        return None
+    
+    @property
+    def database(self):
+        return self.__database
+    
 
 # add in code for a Table class
+import copy
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+    
+    def join(self, other_table, common_key):
+        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+    
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+    
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            temps.append(float(item1[aggregation_key]))
+        return function(temps)
+    
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+    
+    def add(self, _list):
+        if not isinstance(_list, (list)):
+            raise TypeError("_list param must be a list.")
+        for i in _list:
+            if not isinstance(i, (dict)):
+                raise TypeError("One of item is _list is not a dict.")
+        self.table += _list
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
-
-# modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
--- project_manage.py
+++ project_manage.py
@@ -1,9 +1,42 @@
 # import database module
+import database
+from random import randint
+import sys
+import csv
 
 # define a funcion called initializing
 
+def add_user(_list): # person_id, first, last, password, role
+    """
+    Sorry, I defined more functions than specified
+    """
+    password = randint(1000,9999)
+    role = ""
+    if _list[3] == 'student':
+        role = "member"
+    elif _list[3] == 'faculty':
+        role = 'faculty'
+    else:
+        role = 'admin'
+    return {'ID':_list[0], 'username':f"{_list[1]}.{_list[2][0:1]}", 'password':str(password), 'role':role}
+
 def initializing():
-    pass
+    reader = database.CSV_reader()
+    persons_data = reader.read_data_from_file("persons.csv")
+    login_data = reader.read_data_from_file("login.csv")
+    persons = database.Table('persons', persons_data)
+    login_table = database.Table('login', login_data)
+    global database_main
+    database_main = database.DB()
+    database_main.insert(persons)
+    data =[]
+    # # load database
+    # result = []
+    # for i in persons_data:
+    #     result.append(add_user(list(i.values())))
+    # login_table = database.Table('login', result)
+    # print(login_table)
+    database_main.insert(login_table)
 
 # here are things to do in this function:
 
@@ -19,16 +52,39 @@ def initializing():
 # define a funcion called login
 
 def login():
-    pass
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    login_table = database_main.search('login')
+    persons = login_table.select(['ID', 'username', 'password', 'role'])
+    for i in persons:
+        if i['username'] == username:
+            if i['password'] == password:
+                print(i)
+                return i['ID'], i['role']
+    return None
+    
 
 # here are things to do in this function:
    # add code that performs a login task
         # ask a user for a username and password
         # returns [ID, role] if valid, otherwise returning None
 
+def save_csv(name, content):
+    myFile = open(f'{name}.csv', 'w')
+    writer = csv.writer(myFile)
+    writer.writerow(content[0].keys())
+    for dictionary in content:
+        writer.writerow(dictionary.values())
+    myFile.close()
+
 # define a function called exit
 def exit():
-    pass
+    # loop in every table in database
+    for table in database_main.database:
+        # save the file
+        save_csv(table.table_name, database_main.search(table.table_name))
+    sys.exit()
+
 
 # here are things to do in this function:
    # write out all the tables that have been modified to the corresponding csv files
